df2684ed96f05f9f7ce06e65d8c382cc708d350a
Shareenake <User@DESKTOP-TMIVOMJ>
2024-02-24 15:06:47 +0530

    Search and Reset functionality added

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Product } from '../shared/models/products';
 import { HttpClient } from '@angular/common/http';
 import { ShopService } from './shop.service';
@@ -12,6 +12,7 @@ import { ShopParams } from '../shared/models/shopParams';
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit{
+  @ViewChild('search') searchTerm?:ElementRef;
   products:Product[]=[];
   brands:Brand[]=[];
   types:Types[]=[];
@@ -62,11 +63,13 @@ export class ShopComponent implements OnInit{
   onBrandSelected(brandId:number)
   {
     this.shopParams.brandId=brandId;
+    this.shopParams.pageNumber=1;
     this.getProducts();
   }
   onTypeSelected(typeId:number)
   {
     this.shopParams.typeId=typeId;
+    this.shopParams.pageNumber=1;
     this.getProducts();
   }
   onSortSelected(event:any)
@@ -81,6 +84,19 @@ export class ShopComponent implements OnInit{
         this.shopParams.pageNumber=event;
         this.getProducts();   
       }
+
+  }
+  onSerach()
+  {
+    this.shopParams.search=this.searchTerm?.nativeElement.value;
+    this.shopParams.pageNumber=1; 
+    this.getProducts();
+  }
+  onReset()
+  {
+    if(this.searchTerm) this.searchTerm.nativeElement.value='';
+    this.shopParams=new ShopParams();
+    this.getProducts();
 
   }
 }
